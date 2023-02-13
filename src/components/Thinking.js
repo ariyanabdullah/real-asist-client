02c5721a72404c1e9cf82386df67ff8cae255e3d
ariyanabdullah <ak01859136696@gmail.com>
2023-02-14 02:36:02 +0600

    first commit

---
+++ src/components/Thinking.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { MdComputer } from "react-icons/md";
+
+const Thinking = () => {
+  return (
+    <div className="message">
+      <div className="message__wrapper flex">
+        <div className="message__pic">
+          <MdComputer />
+        </div>
+        <div className="text-left message__createdAt">
+          <div className="message__thinking">wait...</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Thinking;
